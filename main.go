008c521a8037cd0a513e171e126c14df03801d7b
0xArch3r <anthony.j.almaguer@gmail.com>
2022-01-31 18:53:05 -0600

    Add Two Handlers, Logging, GitIgnore

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"MicroService-Go/handlers"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+
+	logfile, err := os.OpenFile(".\\microservice-log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Error: Unable to open logfile\n")
+	}
+	defer logfile.Close()
+
+	log_wrt := io.MultiWriter(os.Stdout, logfile)
+	logger := log.New(log_wrt, "[Production-API] ", log.LstdFlags)
+
+	helloHandler := handlers.NewHello(logger)
+	goodbyeHandler := handlers.NewGoodbye(logger)
+	logger.Println("Starting Microservice Application")
+
+	serverMux := http.NewServeMux()
+	serverMux.Handle("/", helloHandler)
+	serverMux.Handle("/goodbye", goodbyeHandler)
+	log.Fatal(http.ListenAndServe(":9090", serverMux))
 }
